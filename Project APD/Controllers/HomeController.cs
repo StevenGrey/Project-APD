63923b579cc51a57a51a9cd77ce244456cf97b9e
Steven <stevengrey45@gmail.com>
2020-04-13 11:48:43 +0200

    Ajoutez des fichiers projet.

---
+++ Project APD/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_APD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult DisplaySkill()
+        {
+            return View("Skills");
+        }
+
+        public ActionResult DisplayPhilo()
+        {
+            return View("Philo");
+        }
+
+        public ActionResult DisplayPartners()
+        {
+            return View("Partners");
+        }
+
+    }
+}
---
+++ Project APD/Controllers/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_APD.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+        public ActionResult DisplayServices()   //Services
+        {
+            return View("Services");
+        }
+
+        public ActionResult DisplayDiy()    //Atelier et Cours de Bricolage
+        {
+            return View("CourseDiy");
+        }
+
+        public ActionResult DisplayNewsEvents() //Actualités et Événements
+        {
+            return View("NewsEvents");
+        }
+
+        public ActionResult DisplayMeeting()    //Rendez-vous
+        {
+            return View("Meeting");
+        }
+    }
+}
