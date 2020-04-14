fda1e8dbec2eae179894a9f397ea06bdac351898
Steven <stevengrey45@gmail.com>
2020-04-14 11:24:24 +0200

    Suppression des Classes Calendriers et ajouts d'un controlleur et des vues pour les services

---
+++ Project APD/Controllers/ServicesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_APD.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InteriorDesign()        //Décoration Intérieur
+        {
+            return View("Interior/Index");
+        }
+
+        public ActionResult ExteriorDesign()        //Décoration Extérieur
+        {
+            return View("Exterior/Index");
+        }
+
+        public ActionResult GoldPlating()           //Dorure à la Feuille d'Or
+        {
+            return View("Gold/Index");
+        }
+
+        public ActionResult GlazingMirrors()        //Vitrerie et Miroiterie 
+        {
+            return View("Glazing/Index");
+        }
+    }
+}
