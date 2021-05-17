7f29feef9806dd0510f96258cb381379b22f5251
Steven Grey <stevengrey@hotmail.fr>
2021-05-17 15:44:09 +0200

    Ajout de nouvelles vues (Décoration Intérieur)

---
+++ Project APD/Controllers/InteriorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_APD.Controllers
+{
+    public class InteriorController : Controller
+    {
+        // GET: Interior
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LivingDesign()        //Salon
+        {
+            return View("Interior/Livingroom");
+        }
+        public ActionResult KitchenDesign()        //Cuisine
+        {
+            return View("Interior/Kitchen");
+        }
+
+        public ActionResult BathDesign()           //Salle de Bain
+        {
+            return View("Interior/Bathroom");
+        }
+
+        public ActionResult BedDesign()            //Chambre
+        {
+            return View("Interior/Bedroom");
+        }
+
+        public ActionResult DiningDesign()        //Salle à Manger
+        {
+            return View("Interior/Diningroom");
+        }
+    }
+}
